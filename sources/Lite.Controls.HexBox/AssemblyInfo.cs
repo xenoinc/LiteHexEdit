7e6dbf8bc418d0e250b01c2cbf445d16d26c8e33
Damian Suess <djsuess@gmail.com>
2026-01-18 11:52:05 -0500

    Version bump and created single-file publishing profile

--- sources/Lite.Controls.HexBox/AssemblyInfo.cs
+++ sources/Lite.Controls.HexBox/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
 
 // In order to sign your assembly you must specify a key to use. Refer to the
 // Microsoft .NET Framework documentation for more information on assembly signing.
